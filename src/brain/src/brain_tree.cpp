bd91e265a229c49517e95f77e17479410e5d1f01
Lucas Reis <reis0932@gmail.com>
2026-07-01 21:36:24 -0300

    fix: Changing behavior to the old version

--- src/brain/src/brain_tree.cpp
+++ src/brain/src/brain_tree.cpp
@@ -756,6 +756,7 @@ NodeStatus Assist::tick() {
     return NodeStatus::SUCCESS;
   }
 
+  /*
   double vxLimit, vyLimit;
   getInput("vx_limit", vxLimit);
   getInput("vy_limit", vyLimit);
@@ -766,6 +767,38 @@ NodeStatus Assist::tick() {
   brain->client->navigateToPoint(targetPose.x, targetPose.y,
                                  vxLimit, vyLimit,
                                  distTolerance, avoidObstacle);
+  */
+
+  double vx, vy, vtheta;
+  auto targetPose_r = brain->data->field2robot(targetPose);
+  double targetDir = atan2(targetPose_r.y, targetPose_r.x);
+  double distToObstacle = brain->distToObstacle(targetDir);
+
+  bool avoidObstacle;
+  brain->get_parameter("obstacle_avoidance.avoid_during_chase", avoidObstacle);
+  double oaSafeDist;
+  brain->get_parameter("obstacle_avoidance.chase_ao_safe_dist", oaSafeDist);
+
+  if (avoidObstacle && distToObstacle < oaSafeDist) {
+    log("avoid obstacle");
+    auto avoidDir = brain->calcAvoidDir(targetDir, oaSafeDist);
+    const double speed = 0.5;
+    vx = speed * cos(avoidDir);
+    vy = speed * sin(avoidDir);
+    vtheta = brain->data->ball.yawToRobot;
+  } else {
+    vx = targetPose_r.x;
+    vy = targetPose_r.y;
+    vtheta = brain->data->ball.yawToRobot * 4.0;
+  }
+
+  double vxLimit, vyLimit;
+  getInput("vx_limit", vxLimit);
+  getInput("vy_limit", vyLimit);
+  vx = cap(vx, vxLimit, -1.0);
+  vy = cap(vy, vyLimit, -vyLimit);
+
+  brain->client->setVelocity(vx, vy, vtheta, false, false, false);
   return NodeStatus::SUCCESS;
 }
 
@@ -1806,7 +1839,7 @@ NodeStatus GoToReadyPosition::tick() {
   brain->client->moveToPoseOnField2(
       tx, ty, ttheta, longRangeThreshold, turnThreshold, vxLimit, vyLimit,
       vthetaLimit, distTolerance / 1.5, distTolerance / 1.5, thetaTolerance,
-      avoidObstacle);
+      false);
   return NodeStatus::SUCCESS;
 }
 
